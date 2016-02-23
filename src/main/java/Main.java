a5bec2a1d7aee7f135b07b6f7264761b6fdc0525
Lauren Dumapias <laurendumapias@Laurens-MacBook-Pro-2.local>
2016-02-23 00:35:09 -0800

    did more steps on heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,11 @@ import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
 import static spark.Spark.get;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
@@ -20,7 +25,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> { RelativisticModel.select();
+	    String energy = System.getenv().get("ENERGY");
+	    Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+	    return "E=mc^2: " + energy + " = " + m.toString();
+	});
     
     get("/ucsb", (req, res) -> "Go Gauchos");
 
